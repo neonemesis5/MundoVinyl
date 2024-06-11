57d9d570a9f6a0017e64d23ec8f23ee96da762c5
Oscar Zambrano <ozambranopinto@gmail.com>
2024-06-11 08:33:27 -0500

    desactivando los endpoint de factura

--- routes/factura.route.js
+++ routes/factura.route.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line import/newline-after-import
 const express = require('express');
 const facturaRouter = express.Router();
-const facturaController = require('../controller/api_factura.controller');
+//const facturaController = require('../controller/api_factura.controller');
 const { autenticarJWT } = require('../middlewares/JWT');
 
-facturaRouter.post('/register',autenticarJWT, facturaController.newfactura);
-facturaRouter.get('/',autenticarJWT, facturaController.getAll);
+//facturaRouter.post('/register',autenticarJWT, facturaController.newfactura);
+//facturaRouter.get('/',autenticarJWT, facturaController.getAll);
 
 module.exports = facturaRouter;
